bfba252c5727783d646e8e0b9d97b17f5bed5610
Satiro07 <jardelsatiro57@gmail.com>
2025-04-03 14:25:21 -0300

    Update log5.py

--- log5.py
+++ log5.py
@@ -1,11 +1,18 @@
+# compreensão de lista
 lista1 = []
-while lista1 != [0]:
-    lista = [int(input('Digite um número: '))]
-    
-    if lista == [0]:
+while True:
+    lista = int(input('Digite um número: '))
+    if lista == 0:
         break
-    lista1 += lista
+    if lista not in lista1:
+        lista1.append(lista)
 print(lista1)
-começo = int(input('Intervalo: '))
-fim = int(input())
-print([n for n in range(começo, fim+1) if n not in lista1])
+comeco = int(input('começo: '))
+comeco1 = comeco
+fim = int(input('fim: '))
+if comeco > fim:
+    comeco = fim
+    fim = comeco1
+    print(f'Números faltantes : {[n for n in range(comeco, fim+1) if n not in lista1]}')
+else:
+    print(f'Números faltantes : {[n for n in range(comeco, fim+1) if n not in lista1]}')
