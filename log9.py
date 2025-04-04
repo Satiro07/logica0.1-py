2ba0a0a528359df2267161f318d81ec84ee62f17
Satiro07 <jardelsatiro57@gmail.com>
2025-04-03 21:31:27 -0300

    Update log9.py

--- log9.py
+++ log9.py
@@ -1,6 +1,4 @@
 frutas = ['Maçã', 'Banana', 'Cereja', 'Manga']
 precos = [2.5, 1.2, 3.0, 4.5]
 
-print(frutas[0] + str(precos[0]))
-
-print([frutas+precos for i in enumerate(frutas) if precos])
+print([frutas[i] + (f': {str(precos[i])}') for i in range(0, len(frutas)) ])
