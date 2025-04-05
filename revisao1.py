d1a4a6615a7307c9d162a2726fdfc81a57f51ee5
Satiro07 <jardelsatiro57@gmail.com>
2025-04-05 20:25:18 -0300

    Update revisao1.py

--- revisao1.py
+++ revisao1.py
@@ -1,12 +1,15 @@
 def media(soma, quantidade):
+    media = soma / quantidade
     if quantidade == 0:
         return 'adicione uma nota!'
-    media = soma / quantidade
-    return media
+    elif media >= 6:
+        return f'Media: {media:.2f} - Aprovado'
+    else:
+        return f'Media: {media:.2f} - Reprovado'
 soma_notas = 0
 quantidade_notas = 0
 while True:
-    n1 = int(input('Digite uma nota: ["-1" para sair] '))
+    n1 = float(input('Digite uma nota: ["-1" para sair] '))
     if n1 < 0:
         break
     soma_notas += n1
