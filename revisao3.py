f62bced3a4db0b22d6f2673808cb5f5590826188
Satiro07 <jardelsatiro57@gmail.com>
2025-04-05 22:12:28 -0300

    Update revisao3.py

--- revisao3.py
+++ revisao3.py
@@ -1,5 +1,8 @@
 numero = int(input('Número: '))
 soma = 1
-for i in range(numero, 1, -1):
-    soma *= i
-print(f'Fatorial de {numero}: {soma}')
+if numero < 0:
+    print(f'O número tem que ser maior ou igual a 0!')
+else:
+    for i in range(numero, 1, -1):
+        soma *= i
+    print(f'Fatorial de {numero}: {soma}')
