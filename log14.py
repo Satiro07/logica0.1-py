e66328ef4d42d1c8281edf21031dcf1eb6249ca2
Satiro07 <jardelsatiro57@gmail.com>
2025-04-15 20:34:21 -0300

    Update log14.py

--- log14.py
+++ log14.py
@@ -1,21 +1,3 @@
-# num = int(input('Digite um valor: '))
-# copia_num = copia = num
-# soma_f = cont =  0
-# while True:
-#     soma = digs = 0
-#     while copia_num:
-#         copia_num //= 10  
-#         digs += 1
-#     if digs == 1:
-#         print(num if cont == 0 else soma_f)
-#         break
-#     for c in range(0, digs):
-#         digito = copia % 10
-#         soma += digito
-#         copia //=10
-#     copia_num = copia = soma_f = soma
-#     cont += 1
-
 def recursao(num):
     copia = num
     soma = 0
