d935017a054b4332c0e7f01b7055d244622946b2
Satiro07 <jardelsatiro57@gmail.com>
2025-04-15 19:48:37 -0300

    Update log11.py

--- log11.py
+++ log11.py
@@ -1,7 +1,12 @@
+# def palindromo(frase):
+#     palavra = ''
+#     for i in range(len(frase)-1, -1, -1):
+#         palavra += frase[i]
+#     return palavra == frase
+# f = 'python'
+# print(palindromo(f))
+
 def palindromo(frase):
-    palavra = ''
-    for i in range(len(frase)-1, -1, -1):
-        palavra += frase[i]
-    return palavra == frase
-f = 'python'
+    return frase == frase[::-1]
+f = 'ana'
 print(palindromo(f))
