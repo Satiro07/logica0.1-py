ac2becffa654655b2db1b5ba4a5d447a824f6d24
Satiro07 <jardelsatiro57@gmail.com>
2025-04-15 22:13:55 -0300

    Update log19.py

--- log19.py
+++ log19.py
@@ -1,11 +1,11 @@
-def frequencia_caracteres(palavra):
+def frequencia_caracteres(lista):
     dic = {}
     cont = 0
-    for i in palavra:
-        dic[i] = palavra.count(i)
+    for i in lista:
+        dic[i] = lista.count(i)
     for k in dic.keys():
         cont += 1
 
-    return  cont
-palavra = [7,7,7,7,7]
+    return cont
+palavra = [1, 2, 3, 2, 4, 1, 5]
 print(frequencia_caracteres(palavra))
