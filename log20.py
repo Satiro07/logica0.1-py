66a329f496c02078d36009c759fee7227d7aae81
Satiro07 <jardelsatiro57@gmail.com>
2025-04-15 22:21:53 -0300

    Update log20.py

--- log20.py
+++ log20.py
@@ -1,17 +1,14 @@
 def sequencia_collatz(numero):
     lista = list()
     lista.append(numero)
-    while numero != 1:
+    while numero > 1:
         if numero % 2 == 0:
             numero /= 2
             lista.append(numero)
         else:
-            numero *=3 +1
+            numero *=3 
+            numero +=1
             lista.append(numero)
     return lista
-
-
-
-
 numero = 6
 print(sequencia_collatz(numero))
