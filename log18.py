d2d9c888c77205a51764a82ca7148eba5240537b
Satiro07 <jardelsatiro57@gmail.com>
2025-04-15 22:06:42 -0300

    Update log18.py

--- log18.py
+++ log18.py
@@ -3,16 +3,17 @@ def verificador_senha(senha):
     letra_minuscula = 0
     espaço = 0
     numero = 0
-    for i in senha:
-        if i == i.lower():
-            letra_minuscula += 1 
-        if ' ' in senha:
-            espaço += 1
-        if i in '1234567890':
-            numero += 1
-        else:
-            if i == i.upper():
-                letra_maiuscula += 1  
+    if len(senha) >= 8:
+        for i in senha:
+            if i == i.lower():
+                letra_minuscula += 1 
+            if ' ' in senha:
+                espaço += 1
+            if i in '1234567890':
+                numero += 1
+            else:
+                if i == i.upper():
+                    letra_maiuscula += 1  
     if letra_maiuscula == 0 or letra_minuscula == 0 or espaço >= 1 or numero == 0:
         return 'Senha inválida'
     else:
