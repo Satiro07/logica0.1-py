c1239a2cfd9ec09509e56e89dbf3a6d2eac608c2
Satiro07 <jardelsatiro57@gmail.com>
2025-04-16 19:17:56 -0300

    Update log21.py

--- log21.py
+++ log21.py
@@ -5,5 +5,5 @@ def quadrados_perfeitos(lista):
             if y * y == i:
                 quadrados.append(i)
     return quadrados
-lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+lista = [2, 3, 5, 7,11]
 print(quadrados_perfeitos(lista))
