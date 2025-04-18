a3a444b14db9d26ee4ea0cadf3ccd92f2bdbd190
Satiro07 <jardelsatiro57@gmail.com>
2025-04-18 10:56:41 -0300

    Update log22.py

--- log22.py
+++ log22.py
@@ -1,13 +1,9 @@
-vetor = [4, 6, 7,9, 3, 9]
-cont = -1
+vetor = [8, 6, 7, 5, 3, 0 ,9]
+c = 0
 for i in range(0, len(vetor)):
-    menor = min(vetor[:cont])
-    vetor.remove(menor)
-    vetor.append(menor)
-    cont -= 1
-    if cont == -len(vetor):
-        vetor.append(vetor[0])
-        vetor.remove(vetor[0])
-        break
+    lei = len(vetor) - c
+    minimo = min(vetor[0:lei])
+    vetor.append(minimo)
+    vetor.remove(minimo)
+    c += 1
 print(vetor)
-        
